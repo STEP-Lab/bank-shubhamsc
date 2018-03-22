731c9e52d9b2a95680e2e65b6eee61e50f77e84b
Subham Singh <s.chauhan5505@gmail.com>
2018-03-22 15:20:40 +0530

    Added test for both transactions

--- library/src/test/java/com/thoughtworks/bank/TransactionsTest.java
+++ library/src/test/java/com/thoughtworks/bank/TransactionsTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.bank;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -8,16 +9,30 @@ import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
 
 public class TransactionsTest {
+
+  private Transactions transactions;
+
+  @Before
+  public void setUp() throws Exception {
+    transactions = new Transactions();
+  }
+
   @Test
   public void mustRecordDebitTransaction() {
-    Transactions transactions = new Transactions();
     transactions.debit(1000,"1234-1234");
     assertThat(transactions.list,hasItem(new DebitTransaction(new Date(),1000,"1234-1234")));
   }
 
   @Test
   public void mustRecordCreditTransaction() {
-    Transactions transactions = new Transactions();
+    transactions.credit(1000,"1234-1234");
+    assertThat(transactions.list,hasItem(new CreditTransaction(new Date(),1000,"1234-1234")));
+  }
+
+  @Test
+  public void mustRecordBothTransactions() {
+    transactions.debit(1000,"1234-1234");
+    assertThat(transactions.list,hasItem(new DebitTransaction(new Date(),1000,"1234-1234")));
     transactions.credit(1000,"1234-1234");
     assertThat(transactions.list,hasItem(new CreditTransaction(new Date(),1000,"1234-1234")));
   }
